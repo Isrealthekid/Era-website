a20dd04469edd97325455bc38718493a58f4e793
isrealthekid <oluwoleisreal12@gmail.com>
2023-10-23 16:16:47 +0200

    background commit

--- index.js
+++ index.js
@@ -63,18 +63,3 @@ var swiper = new Swiper(".mySwiper", {
 
 
 
-
-// ScrollReveal({
-//     reset: true,
-//     distance: '80px',
-//     duration: 2000,
-//     delay: 200
-// });
-
-// ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-
-// ScrollReveal().reveal('.home-img img, .services-container , .portfolio-box, .testimonial-wrapper, .contact form',{ origin: 'bottom' });
-
-// ScrollReveal().reveal('.home-content h1, .about-img img', {origin: 'left'});
-
-// ScrollReveal().reveal('.home-content h3, .home-content p, .about-content', {origin: 'right'});
